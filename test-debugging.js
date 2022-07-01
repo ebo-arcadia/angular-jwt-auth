993fecdf5bdd60b55d94d9790e21a018c2b21eac
Ebo Arcadia <yiboli.coding@gmail.com>
2022-07-01 09:47:26 -0400

    configures node.js debugging using launch.json

--- test-debugging.js
+++ test-debugging.js
@@ -2,7 +2,13 @@ var message = "debug this message";
 var message1 = "debug this message 1";
 var message2 = "debug this message 2";
 var message3 = "debug this message 3";
+
+logger = "this is a logger message";
+
+
 console.log(message);
 console.log(message1);
 console.log(message2);
 console.log(message3);
+
+console.log(logger)
